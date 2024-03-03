54690c6367aa6861341c932af5bc1d8dd5ca065b
Brayan-chan <113928216+Brayan-chan@users.noreply.github.com>
2024-03-02 21:25:26 -0600

    example commit

--- src/exampleortega/ExampleOrtega.java
+++ src/exampleortega/ExampleOrtega.java
@@ -5,6 +5,7 @@ public class ExampleOrtega {
 
     public static void main(String[] args) {
         System.out.println("Esto es un ejemplo para Ortega");
+        System.out.println("hola!");
     }
     
 }
